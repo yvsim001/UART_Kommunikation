797cdbdc296888b681d2a8d8d441ab03cdc3ba4b
Yvan Simo <107270125+yvsim001@users.noreply.github.com>
2023-10-30 10:29:57 +0100

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,43 @@
-#include <Arduino.h>
+#include <avr/io.h>
+#define FOSC 16000000
+#define BAUD 9600
+#define UBRR_Value FOSC/16/BAUD-1
 
-// put function declarations here:
-int myFunction(int, int);
+void USART_Init( uint16_t ubrr)
+{
+  UBRR0H = (uint8_t)(ubrr >>8);
+  UBRR0L = (uint8_t)ubrr;
+  UCSR0B = (1<<RXEN0)|(1<<TXEN0);
+  UCSR0C = (1<<USBS0)|(3<<UCSZ00);
+}
+void USART_Transmit( uint8_t data )
+{
+  while(!(UCSR0A & (1 <<UDRE0)));
+  UDR0 = data;
 
-void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
+uint8_t USART_Receive(void)
+{
+  while(!(UCSR0A & (1<<RXC0)));
+  return UDR0;
 }
+void USART_Flush( void )
+{
+uint8_t dummy;
+while ( UCSR0A & (1<<RXC0) ) dummy = UDR0;
+}
+int main(void)
+{
+  uint8_t data;
+  USART_Init(UBRR_Value);
+  USART_Flush();
+  DDRB= 0xFF;
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+  while(1)
+  {
+    USART_Transmit(data=USART_Receive());
+    PORTB=data;
+  }
+
 }
